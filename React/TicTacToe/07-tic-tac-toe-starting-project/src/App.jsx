fc303f4aca659c53e0caec5942d0666e15ed50ff
Kevin <kevin.ortiz.software@gmail.com>
2025-10-19 19:05:44 -0400

    TicTacToePush

--- React/TicTacToe/07-tic-tac-toe-starting-project/src/App.jsx
+++ React/TicTacToe/07-tic-tac-toe-starting-project/src/App.jsx
@@ -2,14 +2,30 @@ import { useState } from "react";
 
 import GameBoard from "./components/Gameboard.tsx";
 import Player from "./components/Player.tsx";
+import Log from "./components/Log.tsx";
 
 // {/* even though we have component Player for both, these are 2 separate instances, so Player 1 instance will be DIFFERENt than Player 2 instance */}
 
 export default function App() {
+  const [gameTurns, setGameTurns] = useState([]);
+  // use this useState array instead of the func inside of GameBoard.tsx to keep track of current state / change of state
+
+
   const [activePlayer, setActivePlayer] = useState("X");
 
-  function handleSelectSquare() {
-    setActivePlayer( (curActivePlayer) => curActivePlayer === 'X' ? 'O': 'X')
+  function handleSelectSquare(rowIndex, colIndex) {
+    setActivePlayer( (curActivePlayer) => curActivePlayer === 'X' ? 'O': 'X');
+    setGameTurns(prevTurns => {
+      let currentPlayer = 'X';
+
+      if (prevTurns.length > 0 && prevTurns[0].player === 'X') { // prevTurns[0] will always be previous turn
+        currentPlayer = 'O';
+      }
+      const updatedTurns = [{square: {row: rowIndex, col: colIndex}, player: currentPlayer }, ...prevTurns];
+
+      return updatedTurns;
+
+    });
   }
 
   return (
@@ -22,7 +38,7 @@ export default function App() {
         </ol>
         <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
       </div>
-      LOG
+      <Log/>
     </main>
   );
 }
---
+++ React/TicTacToe/07-tic-tac-toe-starting-project/src/components/Log.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+export default function Log() {
+
+useState
+
+
+
+
+    return <ol id="log">
+        
+    </ol>
+}
