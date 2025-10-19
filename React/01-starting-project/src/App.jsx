1d3a7ac045acaa25b9bb00538b530d6dae6185c0
Kevin <kevin.ortiz.software@gmail.com>
2025-10-19 15:39:54 -0400

    Pushing starting project for React

--- React/01-starting-project/src/App.jsx
+++ React/01-starting-project/src/App.jsx
@@ -1,76 +1,81 @@
-import MainGoal from "./MainGoal";
-import reactImg from "./assets/react-core-concepts.png";
+import { useState } from 'react';
 
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)]
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
+import { CORE_CONCEPTS } from './data.js';
+import Header from './components/Header/Header.jsx';
+import CoreConcept from './components/CoreConcept.jsx';
+import TabButton from './components/TabButton.jsx';
+import { EXAMPLES } from './data.js';
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  function handleSelect(selectedButton) {
+    // selectedButton => 'components', 'jsx', 'props', 'state'
+    setSelectedTopic(selectedButton);
+    // console.log(selectedTopic);
+  }
+
+  console.log('APP COMPONENT EXECUTING');
+
+  let tabContent = <p>Please select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
+
   return (
     <div>
-      {/* <Header></Header>  */}
-      {/* can use self-closing tag */}
       <Header />
       <main>
-        <h2>Time to get started!</h2>
-        <MainGoal />
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            {CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcept key={conceptItem.title} {...conceptItem} />
+            ))}
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton
+              isSelected={selectedTopic === 'components'}
+              onSelect={() => handleSelect('components')}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === 'jsx'}
+              onSelect={() => handleSelect('jsx')}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === 'props'}
+              onSelect={() => handleSelect('props')}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === 'state'}
+              onSelect={() => handleSelect('state')}
+            >
+              State
+            </TabButton>
+          </menu>
+          {tabContent}
+        </section>
       </main>
     </div>
   );
 }
-
-/**
- * import React from 'react';
-
-export const userData = {
-  firstName: 'Maximilian', // feel free to replace the name value
-  lastName: 'Schwarzmüller', // feel free to replace the name value
-  title: 'Instructor', // feel free to replace the title value
-};
-
-// Edit the User component code to output the userData data
-export function User() {
-  return (
-    <div id="user" data-testid="user">
-      <h2>
-        TODO: FIRST NAME LAST NAME
-      </h2>
-      <p>TODO: TITLE</p>
-    </div>
-  );
-}
-
-// DON'T edit the App component code
-function App() {
-  return (
-    <div id="app">
-      <h1>Time to Practice</h1>
-      <p>Welcome on board of this course! You got this 💪</p>
-      <User />
-    </div>
-  );
-}
-
-export default App;
-
- */
 
 export default App;
